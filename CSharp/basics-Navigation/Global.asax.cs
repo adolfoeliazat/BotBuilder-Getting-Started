116e550d4a078b70a8428447f57f1da0f9210cd0
Chris Mayo <cmayo@microsoft.com>
2017-06-14 12:42:49 -0700

    Wired up registration of navigation module.

--- CSharp/basics-Navigation/Global.asax.cs
+++ CSharp/basics-Navigation/Global.asax.cs
@@ -1,17 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Http;
-using System.Web.Routing;
-
-namespace NavigationBot
+﻿namespace NavigationBot
 {
+    using System.Web.Http;
+    using Autofac;
+    using Microsoft.Bot.Builder.Dialogs;
+    using Microsoft.Bot.Builder.Internals.Fibers;
+    using Navigation;
+
     public class WebApiApplication : System.Web.HttpApplication
     {
         protected void Application_Start()
         {
+            this.RegisterModules();
+
             GlobalConfiguration.Configure(WebApiConfig.Register);
+        }
+
+        private void RegisterModules()
+        {
+            var builder = new ContainerBuilder();
+
+            builder.RegisterModule(new ReflectionSurrogateModule());
+
+            // Wire up all global navigation commands via scorables.
+            builder.RegisterModule<NavigationModule>();
+
+            builder.Update(Conversation.Container);
         }
     }
 }
